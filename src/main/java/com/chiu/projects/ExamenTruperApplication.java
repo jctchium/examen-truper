2229fff57b30d02a64c5979c1f82e95856bae93e
José Carlos Chiu <jctchium@msn.com>
2025-12-22 11:13:25 -0600

    Habilitar H2
    Habilitar H2

---
+++ src/main/java/com/chiu/projects/ExamenTruperApplication.java
@@ -0,0 +1,13 @@
+package com.chiu.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenTruperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenTruperApplication.class, args);
+	}
+
+}
